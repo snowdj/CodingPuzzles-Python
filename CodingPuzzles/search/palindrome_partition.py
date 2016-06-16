b2699ae0ae8fddc3e8675bb637ef3ba9409e384b
euccas <euccas.chen@gmail.com>
2016-06-15 18:01:28 -0700

    Add Palindrome Partition

---
+++ CodingPuzzles/search/palindrome_partition.py
@@ -0,0 +1,33 @@
+result = []
+
+def isPalindrome(s):
+    for i in range(len(s)):
+        if s[i] != s[len(s) -1 -i]:
+            return False
+    return True
+
+def dfs_visit(s, stringlist):
+    global result
+    if len(s) == 0:
+        print "stringlist: ", stringlist
+        result.append(stringlist)
+        print "result: ", result
+    for i in range(1, len(s) + 1):
+        if isPalindrome(s[:i]):
+           dfs_visit(s[i:], stringlist + [s[:i]])
+
+def partition(s):
+    global result
+    dfs_visit(s, [])
+    print result
+
+def test():
+    s = 'aab'
+    print "Test Data: "
+    print s
+
+    print "Test Result: "
+    partition(s)
+
+if __name__ == "__main__":
+    test()
