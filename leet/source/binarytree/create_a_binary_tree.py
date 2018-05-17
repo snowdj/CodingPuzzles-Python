b02902d7fd4cd27f7beaa8644366abd384dc19dc
euchen <euchen@qti.qualcomm.com>
2018-05-17 16:31:38 -0700

    update binary tree exercises

--- leet/source/binarytree/create_a_binary_tree.py
+++ leet/source/binarytree/create_a_binary_tree.py
@@ -57,39 +57,7 @@ class BinaryTree():
         self.postorder_helper(node.right)
         self.seq_postorder.append(node.val)
 
-
-class Solution():
-
-    def findSubtree(self, root):
-        # write your code here
-        import sys
-        self.min_sum = sys.maxint
-        self.result = None
-
-        self.search(root)
-        return self.result
-
-    def search(self, node):
-        if node is None:
-            return 0
-
-        left_sum = self.search(node.left)
-        right_sum = self.search(node.right)
-
-        curr_sum = left_sum + right_sum + node.val
-        if curr_sum <= self.min_sum:
-            self.min_sum = curr_sum
-            self.result = node
-
-        return curr_sum
-
 if __name__ == "__main__":
     tree = BinaryTree();
     result = tree.preorder()
     print(result)
-
-    solution = Solution();
-    result = solution.findSubtree(tree.root)
-    print(result)
-
-
