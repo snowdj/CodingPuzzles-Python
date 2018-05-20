179f6e47caac991b2184c2e1d8b06f2c28767dd1
euccas <euccas.chen@gmail.com>
2018-05-19 17:38:21 -0700

    Add binary tree path

---
+++ leet/source/binarytree/binary_tree_path.py
@@ -0,0 +1,93 @@
+class Solution():
+    def binaryTreePaths(self, root):
+        if root is None:
+            return []
+
+        result = []
+        self.dfs(root, result, [])
+        return result
+
+    def dfs(self, node, result, curr_path):
+        curr_path.append(str(node.val))
+
+        if node.left is None and node.right is None:
+            result.append('->'.join(curr_path))
+            curr_path.pop()
+            return
+
+        if node.left:
+            self.dfs(node.left, result, curr_path)
+
+        if node.right:
+            self.dfs(node.right, result, curr_path)
+
+        curr_path.pop()
+
+
+class Node():
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class BinaryTree():
+    def __init__(self):
+        self.root = Node(5)
+        node1_0 = Node(2)
+        node1_1 = Node(3)
+        node2_0 = Node(-1)
+        node2_1 = Node(4)
+        node2_2 = Node(2)
+        node2_3 = Node(0)
+        self.root.left = node1_0
+        self.root.right = node1_1
+        node1_0.left = node2_0
+        node1_0.right = node2_1
+        node1_1.left = node2_2
+        node1_1.right = node2_3
+
+        self.seq_preorder = []
+        self.seq_inorder = []
+        self.seq_postorder = []
+
+    def preorder(self):
+        self.preorder_helper(self.root)
+        return self.seq_preorder
+
+    def preorder_helper(self, node):
+        if node is None:
+            return
+        self.seq_preorder.append(node.val)
+        self.preorder_helper(node.left)
+        self.preorder_helper(node.right)
+
+    def inorder(self):
+        self.inorder_helper(self.root)
+        return self.seq_inorder
+
+    def inorder_helper(self, node):
+        if node is None:
+            return
+        self.inorder_helper(node.left)
+        self.seq_inorder.append(node.val)
+        self.inorder_helper(node.right)
+
+    def postorder(self):
+        self.postorder_helper(self.root)
+        return self.postorder
+
+    def postorder_helper(self, node):
+        if node is None:
+            return
+        self.postorder_helper(node.left)
+        self.postorder_helper(node.right)
+        self.seq_postorder.append(node.val)
+
+if __name__ == "__main__":
+    tree = BinaryTree();
+    result = tree.preorder()
+    print(result)
+
+    sln = Solution();
+    result = sln.binaryTreePaths(tree.root)
+    print(result)
---
+++ leet/source/binarytree/closest_binary_search_tree_value.py
@@ -0,0 +1,67 @@
+# O(n), n is the number of nodes in the binary tree
+class Solution():
+    def closestValue(self, root, target):
+        # write your code here
+        if root is None or target is None:
+            return None
+
+        import sys
+        self.min_diff = sys.maxint
+        self.result = None
+        self.search(root, target)
+        return self.result.val
+
+    def search(self, node, target):
+        if node is None:
+            return
+
+        curr_diff = abs(node.val - target)
+        if curr_diff < self.min_diff:
+            self.min_diff = curr_diff
+            self.result = node
+
+        self.search(node.left, target)
+        self.search(node.right, target)
+
+# O(h), h is the height of the binary tree
+class Solution1():
+    def cloestValue(self, root, target):
+        if root is None or target is None:
+            return None
+
+        lowerNode = self.lowerBound(root, target)
+        upperNode = self.upperBound(root, target)
+
+        if lowerNode is None:
+            return upperNode.val
+        if upperNode is None:
+            return lowerNode.val
+        if target - lowerNode.val > upperNode.val - target:
+            return upperNode.val
+        return lowerNode.val
+
+    def lowerBound(self, node, target):
+        if node is None:
+            return None
+
+        if target <= node.val:
+            return self.lowerBound(node.left, target)
+
+        lowerNode = self.lowerBound(node.right, target)
+        if lowerNode is not None:
+            return lowerNode
+
+        return node
+
+    def upperBound(self, node, target):
+        if node is None:
+            return None
+
+        if target >= node.val:
+            return self.upperBound(node.right, target)
+
+        upperNode = self.upperBound(node.left, target)
+        if upperNode is not None:
+            return upperNode
+
+        return node
