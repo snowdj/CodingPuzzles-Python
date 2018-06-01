5540b1d0379116961c40d921fed23be2b392eca1
euccas <euccas.chen@gmail.com>
2018-06-01 00:15:02 -0700

    Update combination DFS exercises (backtracking)

--- leet/source/searchDFS/palindrome_partition.py
+++ leet/source/searchDFS/palindrome_partition.py
@@ -10,7 +10,7 @@ def dfs_visit(s, stringlist):
     global result
 
     if len(s) == 0:
-        print "stringlist: ", stringlist
+        print("stringlist: ", stringlist)
         result.append(stringlist)
 
     for i in range(1, len(s) + 1): # when i == len(s), s[i:] is empty string, which will cause result be updated
@@ -20,14 +20,14 @@ def dfs_visit(s, stringlist):
 def partition(s):
     global result
     dfs_visit(s, [])
-    print result
+    print(result)
 
 def test():
     s = 'aab'
-    print "Test Data: "
-    print s
+    print("Test Data: ")
+    print(s)
 
-    print "Test Result: "
+    print("Test Result: ")
     partition(s)
 
 if __name__ == "__main__":
